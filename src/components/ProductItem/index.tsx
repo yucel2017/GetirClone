40a934fb2237cf09b810cdb43be9251bb2145eb1
Yücel Danışman <126460768+yucel2017@users.noreply.github.com>
2023-11-30 23:23:04 +0300

    sixth

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -2,23 +2,29 @@ import { View, Text, TouchableOpacity, Image, Dimensions } from 'react-native'
 import React from 'react'
 import { Entypo } from '@expo/vector-icons';
 import { Product } from '../../models';
+import { useNavigation } from '@react-navigation/native';
 
 const { width, height } = Dimensions.get('window')
-type productItemType={
-    item:Product
+type productItemType = {
+    item: Product
 }
 
 
-function index({item}:productItemType) {
+function index({ item }: productItemType) {
+
+    const navigation = useNavigation()
+
     return (
-        <TouchableOpacity style={{
-            width: width * 0.28,
-            marginTop: 12,
-            //backgroundColor: 'red',
-            height: height * 0.24,
-            marginLeft: 12,
-            marginBottom: 6
-        }} >
+        <TouchableOpacity
+            onPress={() => navigation.navigate("ProductDetails", { product: item })}
+            style={{
+                width: width * 0.28,
+                marginTop: 12,
+                //backgroundColor: 'red',
+                height: height * 0.24,
+                marginLeft: 12,
+                marginBottom: 6
+            }} >
             <Image style={{ width: width * 0.28, height: width * 0.28, borderRadius: 12, borderWidth: 0.1, borderColor: 'gray' }} source={{ uri: item.image }} />
             <View style={{ flexDirection: 'row', marginTop: 10, alignItems: 'center' }} >
                 <Text style={{ fontSize: 11.4, color: "#747990", textDecorationLine: 'line-through' }} >
@@ -35,7 +41,7 @@ function index({item}:productItemType) {
             </View>
             <Text style={{ fontSize: 12, fontWeight: '600', marginTop: 5 }} >{item.name}</Text>
             <Text style={{ color: '#747990', fontSize: 12, marginTop: 4, fontWeight: '500' }} >{item.miktar} </Text>
-            <View style={{ alignItems: 'center', justifyContent: 'center',shadowRadius:3.8,shadowOpacity:0.05, width: 30, height: 30, borderWidth: 0.3, borderColor: 'lightgrey', backgroundColor: 'white', position: 'absolute', right: -6, top: -6, borderRadius: 6 }} >
+            <View style={{ alignItems: 'center', justifyContent: 'center', shadowRadius: 3.8, shadowOpacity: 0.05, width: 30, height: 30, borderWidth: 0.3, borderColor: 'lightgrey', backgroundColor: 'white', position: 'absolute', right: -6, top: -6, borderRadius: 6 }} >
                 <Entypo name="plus" size={22} color="#5D3EBD" />
             </View>
         </TouchableOpacity>
--- src/navigators/HomeNavigator.tsx
+++ src/navigators/HomeNavigator.tsx
@@ -1,12 +1,29 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from '../screens/HomeScreen'
 import CategoryFilterScreen from '../screens/CategoryFilterScreen'
-
+import ProductDetailsScreen from '../screens/ProductDetailsScreen'
+import { Ionicons, Foundation } from '@expo/vector-icons'
+import { useNavigation, getFocusedRouteNameFromRoute } from '@react-navigation/native'
 const Stack = createStackNavigator()
 
-function HomeNavigator() {
+function MyStack({ navigation, route }) {
+
+
+    const tabHiddenRoutes = ["ProductDetails"];
+
+    React.useLayoutEffect(() => {
+        const routeName = getFocusedRouteNameFromRoute(route);
+        console.log("Route Name is ", routeName);
+        if (tabHiddenRoutes.includes(routeName)) {
+            console.log("Kapat ", routeName);
+            navigation.setOptions({ tabBarStyle: { display: "none" } });
+        } else {
+            console.log("Aç ", routeName);
+            navigation.setOptions({ tabBarStyle: { display: "true" } });
+        }
+    }, [navigation.route])
 
     return (
 
@@ -29,15 +46,40 @@ function HomeNavigator() {
                 name='CategoryDetails'
                 component={CategoryFilterScreen}
                 options={{
-                    headerBackTitleVisible:false,
-                    headerTintColor:'white',
+                    headerBackTitleVisible: false,
+                    headerTintColor: 'white',
                     headerStyle: { backgroundColor: "#5C3EBC" },
                     headerTitle: () => (
-                        <Text style={{fontWeight:'bold',fontSize:15,color:'white' }} >
+                        <Text style={{ fontWeight: 'bold', fontSize: 15, color: 'white' }} >
                             Ürünler
                         </Text>
                     )
                 }}
+            />
+
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: "#5C3EBC" },
+                    headerBackTitleVisible: false,
+                    headerTintColor: 'white',
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingLeft: 12 }} >
+                            <Ionicons name="close" size={24} color="white" />
+                        </TouchableOpacity>
+                    ),
+                    headerTitle: () => (
+                        <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 15 }} >
+                            Ürün Detayı
+                        </Text>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity style={{ paddingRight: 10 }} >
+                            <Foundation name='heart' size={24} color="#32177a" />
+                        </TouchableOpacity>
+                    )
+                }}
+                name='ProductDetails'
+                component={ProductDetailsScreen}
             />
 
         </Stack.Navigator>
@@ -45,4 +87,6 @@ function HomeNavigator() {
     )
 }
 
-export default HomeNavigator
+export default function HomeNavigator({ navigation, route }) {
+    return <MyStack navigation={navigation} route={route} />
+}
